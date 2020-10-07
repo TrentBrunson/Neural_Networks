a8a997f8200bad9820ece9d2545f9f319e07105d
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-10-06 19:25:42 -0500

    hr scaled and transformed

--- working/hr.py
+++ working/hr.py
@@ -7,3 +7,17 @@ from sklearn.preprocessing import StandardScaler
 hr_df = pd.read_csv("hr_dataset.csv")
 hr_df.head()
 # %%
+# scale the data
+# Create the StandardScaler instance
+scaler = StandardScaler()
+# %%
+# Fit the StandardScaler
+scaler.fit(hr_df)
+# %%
+# Scale the data
+scaled_data = scaler.transform(hr_df)
+# %%
+# Create a DataFrame with the scaled data
+transformed_scaled_data = pd.DataFrame(scaled_data, columns=hr_df.columns)
+transformed_scaled_data.head()
+# %%
