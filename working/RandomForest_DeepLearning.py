8128d6165d7f87dc01ce180f3fe05912f7c085b8
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-10-11 13:23:33 -0500

    split, fit and scaled the data

--- working/RandomForest_DeepLearning.py
+++ working/RandomForest_DeepLearning.py
@@ -49,3 +49,24 @@ loans_df = loans_df.merge(encode_df,left_index=True, right_index=True)
 loans_df = loans_df.drop(loans_cat,1)
 loans_df.head()
 # %%
+# split data for training and testing
+
+# Remove loan status target from features data
+y = loans_df.Loan_Status_Fully_Paid
+X = loans_df.drop(columns=["Loan_Status_Fully_Paid","Loan_Status_Not_Paid"]).values
+
+# Split training/test datasets
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y)
+
+# Create a StandardScaler instance
+scaler = StandardScaler()
+
+# Fit the StandardScaler
+X_scaler = scaler.fit(X_train)
+
+# Scale the data
+X_train_scaled = X_scaler.transform(X_train)
+X_test_scaled = X_scaler.transform(X_test)
+# %%
+# after splitting data, it is ready for evaluation in both models
+
